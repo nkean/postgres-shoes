13f458a268f2ae0b96789abf3ff89b4a7d9812de
nkean <nathankean2@gmail.com>
2018-05-03 11:12:28 -0500

    database connection working

--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'shoe_store',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', () => {
+    console.log(`Postgresql connected`);
+});
+
+pool.on('error', (error) => {
+    console.log(`Error with postgres pool: ${error}`);
+});
+
+const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 const shoes = [
@@ -45,8 +66,16 @@ app.get('/shoe', (req, res) => {
 });
 
 app.post('/shoe', (req, res) => {
-    shoes.push(req.body);
-    res.sendStatus(200);
+    const shoe = req.body;
+    pool.query(`INSERT INTO "shoes" ("name", "cost")
+                VALUES ($1, $2);`, [shoe.name, shoe.cost]) // data sanitization!
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(`error with SQL INSERT on POST to /shoe: ${error}`);
+            res.sendStatus(500);
+        });
 });
 
 app.listen(PORT, () => {
